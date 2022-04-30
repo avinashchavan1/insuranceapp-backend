40abeb71378c32222c75ddb6235e6d3d331fccf6
Avinash Chavan <avinashchavan1443@gmail.com>
2022-04-30 13:15:39 +0530

    update policy endpoint

--- routes/Policy.routes.js
+++ routes/Policy.routes.js
@@ -14,8 +14,7 @@ router.get("/userId/:id", async (req, res, next) => {
   const policy = await Policy.find({ customer_id: user._id }).populate(
     "customer_id"
   );
-  console.log(policy.length);
-
+  console.log("Number of Policies :", policy.length);
   res.json(policy).status(200);
 });
 
@@ -24,4 +23,42 @@ router.get("/policyId/:id", async (req, res, next) => {
   const policy = await Policy.find({ id: policyId }).populate("customer_id");
   res.json(policy).status(200);
 });
+
+router.post("/update", async (req, res, next) => {
+  const policyDetails = req.body;
+  const policy = await Policy.findOne({ id: policyDetails.id }).populate(
+    "customer_id"
+  );
+  if (policy === null) {
+    return res.status(400).json({ message: "Policy not found" });
+  }
+  if (policy.date_of_purchase !== policyDetails.date_of_purchase) {
+    return res
+      .status(400)
+      .json({ message: "Modification Restricted : Date of Purchase" });
+  }
+  if (policyDetails.premium > 1000000) {
+    return res.status(400).json({ message: "Premium is too high" });
+  }
+  const user = await User.findOne({ id: policyDetails.customer_id.id });
+  policy.customer_id = user;
+  policy.id = policyDetails.id;
+  policy.date_of_purchase = policyDetails.date_of_purchase;
+  policy.vechile_segment = policyDetails.vechile_segment;
+  policy.fuel = policyDetails.fuel;
+  policy.premium = policyDetails.premium;
+  policy.bodily_injury_liability = policyDetails.bodily_injury_liability;
+  policy.personal_injury_protection = policyDetails.personal_injury_protection;
+  policy.property_damage_liability = policyDetails.property_damage_liability;
+  policy.collision = policyDetails.collision;
+  policy.comprehensive = policyDetails.comprehensive;
+  console.log(user);
+  await policy
+    .save()
+    .then((result) => res.status(200).json(policy))
+    .catch((err) =>
+      res.status(400).json({ message: "Couldnt update policy", message: err })
+    );
+});
+
 module.exports = router;
